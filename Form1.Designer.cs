9addb95038e8b259a433901d721ba0fcb0fe34a8
zneiat <1149527164@qq.com>
2017-01-08 10:32:45 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace EnglishWeeklyDaSearchTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "提取码";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(254, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "期数";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "I AM Luckly";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(162, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "好自为之 2016.12.17";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(313, 23);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(371, 161);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(320, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "加载中.. 稍等一下 (/≧▽≦)/";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(710, 214);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "English Weekly DA Search Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.textBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using DotNet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace EnglishWeeklyDaSearchTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("提取码不能为空");
+                return;
+            }
+            // 发送请求
+            HttpHelper httpKey = new HttpHelper();
+            HttpItem itemKey = new HttpItem()
+            {
+                URL = "http://test.airlind.net/check.html",
+                Method = "GET",
+                Encoding = Encoding.UTF8,
+                ResultType = ResultType.String
+            };
+            //得到HTML代码
+            HttpResult resultKey = httpKey.GetHtml(itemKey);
+            string htmlKey = resultKey.Html;
+            if (resultKey.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                MessageBox.Show("放弃吧，骚年，工具已废");
+            }
+            if (textBox1.Text.Trim() != htmlKey)
+            {
+                MessageBox.Show("提取码不正确");
+                return;
+            }
+            label4.Visible = true;
+            string whatNum = textBox2.Text.Trim();
+            if (int.Parse(whatNum) < 10)
+            {
+                whatNum = "0" + whatNum;
+            }
+            if (int.Parse(whatNum) > 99)
+            {
+                MessageBox.Show("没有可以找到的答案了");
+            }
+            if (int.Parse(whatNum) < 1)
+            {
+                MessageBox.Show("什么鬼？！");
+            }
+            // 发送请求
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem()
+            {
+                URL = "http://app.ew.com.cn/Weekly/index.php?c=ResourceController&a=getResourceById&id=00538023"+ whatNum + "80&from=true",
+                Method = "GET",
+                Encoding = Encoding.UTF8,
+                ResultType = ResultType.String
+            };
+            //得到HTML代码
+            HttpResult result = http.GetHtml(item);
+            string html = result.Html;
+            Regex idRegex = new Regex("\"resource_path\":\"(.*?)\"");
+            string idData = idRegex.Match(html).Groups[1].Value;
+            // 请求2
+            // 去除转义字符
+            idData = idData.Replace("\\", "");
+            // 发送请求
+            HttpHelper http2 = new HttpHelper();
+            HttpItem item2 = new HttpItem()
+            {
+                URL = idData+"/question.txt",
+                Method = "GET",
+                Encoding = Encoding.UTF8,
+                ResultType = ResultType.String
+            };
+            HttpResult result2 = http.GetHtml(item2);
+            string html2 = result2.Html;
+            textBox3.Text = html2;
+            label4.Visible = false;
+        }
+    }
+}
